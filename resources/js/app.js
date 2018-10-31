686c3c3b264d65165fc8a909307c3f197b9188bf
Yudi Andela <yudhi.andhela@gmail.com>
2018-10-31 23:29:35 +0700

    Menambahkan task-component

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('task-component', require('./components/TaskComponent.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/js/components/TaskComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-10 offset-md-1">
+                <div class="card rounded-0">
+                    <div class="card-header">
+                        Task List
+                        <button class="btn btn-primary btn-sm rounded-0 float-right">Add New Task</button>
+                    </div>
+
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Task Name</th>
+                                    <th>Priority</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Nama Task</td>
+                                    <td>high</td>
+                                    <td>
+                                        <a href="#" class="btn btn-secondary btn-sm rounded-0">Edit</a>
+                                        <a href="#" class="btn btn-danger btn-sm rounded-0">Delete</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
